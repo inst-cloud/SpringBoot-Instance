3fb7ccbaf4134b44872477f5e6e2419978b9da12
wudi <291100854@qq.com>
2021-04-01 16:21:34 +0800

    简单 添加配置

--- security-http-basic/src/main/java/com/inst/springboot/security/httpbasic/config/SecurityConfig.java
+++ security-http-basic/src/main/java/com/inst/springboot/security/httpbasic/config/SecurityConfig.java
@@ -14,7 +14,11 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-
+            http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();//所有请求都需要登录认证才能访问
         }
 
 }
